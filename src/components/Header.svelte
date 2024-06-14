db1fa13af75d85efb474e4d57466eb007909ecea
chessboy71 <bentegrimohamed@gmail.com>
2024-06-14 04:17:31 +0200

    First commit, Making header and hero Section

---
+++ src/components/Header.svelte
@@ -0,0 +1,39 @@
+<script>
+   export let y;
+
+   let tabs = [
+      {
+         name: "Projects",
+         link: "#project"
+      },
+      {
+         name: "About me",
+         link: "#about"
+      }
+   ]
+</script>
+
+<header class={'sticky z-[10] top-3 duration-200 px-6 flex items-center justify-between border-b border-solid' + (
+   y > 0 ? 
+   'py-4 bg-slate-950 border-violet-950': 
+   'py-6 bg-transparent border-transparent'
+   )}>
+   
+   <h2 class=" font-light text-xl">
+      TheMaze <span class="font-bold poppins">Project</span>
+   </h2>
+   <div class="sm:flex items-center gap-4 hidden">
+      {#each tabs as tab, index}
+         <a class="duration-200 hover:text-violet-400">
+            <p>{tab.name}</p>
+         </a>
+      {/each}
+   </div>
+   <a href="#"
+      target="_blank"
+      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+      <div class=" absolute top-0 w-full right-full h-full 
+      bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+      <h4 class="relative z-9 poppins font-medium text-sm">Get in touch</h4>
+   </a>
+</header>
---
+++ src/components/Main.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+   <section id="intro" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 min-h-screen">
+      <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+         <h2
+         class="font-black text-4xl sm:text-5xl md:text-6xl">
+         Hello! this is the <span class="text-violet-400 poppins">Maze Project</span> <br>
+         This project is part of the <span class="text-violet-400 poppins"> Alx program</span>
+         </h2>
+         <p class="text-base sm:text-lg md:text-xl poppins font-medium text-gray-300">
+            The goal of this project is to create a game in 3D using raycasting!
+            It is made used the library <span class="text-violet-300">SDL2 with c.</span>
+         </p>
+         <a
+            class="blueShadow mr-auto lg:mr-auto text-lg sm:text-lg md:text-4xl poppins relative overflow-hidden
+            px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+            href="#"
+            target="_blank">
+            <div class="absolute top-0 w-full left-0 h-full
+            bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+            <h4 class="relative z-9 poppins font-semibold text-lg">See the repo</h4>
+
+         </a>
+      </div>
+   </section>
+</main>
